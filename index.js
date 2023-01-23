96a9b77d0defd5a90939e220d46f52904e6189c9
KonradK <konrad.0808.k@gmail.com>
2023-01-23 23:29:37 +0100

    Change load of cardList.

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 // import fetchData from "./fetchData";
 // import displayCards from "./displayCards";
 
-// fetchData();
-// displayCards();
 const pokeDataArr = [];
-const fetchData = async function() {
-    try {
-        for (let i=1; i<=386; i++){
-            let data1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-            let resp = await data1.json();
-            let pokeObj = {
-                id: resp.id,
-                name: resp.name,
-                spriteLink: resp.sprites.front_default,
-                type1: resp.types[0].type.name,
-                type2:(resp.types.length > 1 ? resp.types[1].type.name : null)
-            }
-            console.log('Pokemon:', pokeObj);
-            // const pokeArr = Object.keys(pokeObj); // Obj to Arr
-            pokeDataArr.push(pokeObj);
-        }
-    } catch(err) {
-        console.log("Oops!", err);
-    }
-    finally {
-        console.log('OUTPUT:', pokeDataArr);
-    }
-}
 function addCard(el) {
     const cardList = document.querySelector('.card-list');
     const card = document.createElement("div");
@@ -47,13 +22,27 @@ function addCard(el) {
     card.appendChild(cardDiv)
     cardList.appendChild(card);
 }
-const displayCards = function() {
-    for (const element of pokeDataArr) {
-        addCard(element);
+const fetchData = async function() {
+    try {
+        for (let i=1; i<=385; i++){
+            let data1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+            let resp = await data1.json();
+            let pokeObj = {
+                id: resp.id,
+                name: resp.name,
+                spriteLink: resp.sprites.front_default,
+                type1: resp.types[0].type.name,
+                type2:(resp.types.length > 1 ? resp.types[1].type.name : null)
+            }
+            pokeDataArr.push(pokeObj);
+            await addCard(pokeObj);
+            // console.log('Pokemon:', pokeObj);
+        }
+    } catch(err) {
+        console.log("Oops!", err);
+    }
+    finally {
+        console.log('fetchData end!');
     }
 }
-async function refreshList() {
-   await fetchData();
-   await displayCards();
-}
-refreshList();
+fetchData();
